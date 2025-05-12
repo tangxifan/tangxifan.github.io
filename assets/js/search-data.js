76108f6f0fce6e7b5c9d2ca4e42ddfd40ffed0c6
tangxifan <tangxifan@gmail.com>
2025-05-12 00:27:56 +0000

    Deploying to gh-pages from @ tangxifan/tangxifan.github.io@835d1aeba8a2a24d1d706f5b2d89733c5fa0fa30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%69%66%61%6E.%74%61%6E%67@%69%65%65%65.%6F%72%67", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +509,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/xifantang", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +529,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=8xx9aw0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
